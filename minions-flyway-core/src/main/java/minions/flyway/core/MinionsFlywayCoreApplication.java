158fef7c69be3e34e3329d6dbfcfafb6bb4d94ba
fubo <groooooomit@gmail.com>
2019-07-19 10:47:26 +0800

    1、Create empty project 'minions-flyway'. 2、Add new module 'minions-flyway-core' to project 'minions-flyway'.

---
+++ minions-flyway-core/src/main/java/minions/flyway/core/MinionsFlywayCoreApplication.java
@@ -0,0 +1,13 @@
+package minions.flyway.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinionsFlywayCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinionsFlywayCoreApplication.class, args);
+	}
+
+}
