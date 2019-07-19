c360cebeebb5cab929a0db48590dbd1dfd56bc92
fubo <groooooomit@gmail.com>
2019-07-19 10:55:47 +0800

    Add new module 'minions-flyway-demo' to project 'minions-flyway'.

---
+++ minions-flyway-demo/src/main/java/minions/flyway/demo/MinionsFlywayDemoApplication.java
@@ -0,0 +1,13 @@
+package minions.flyway.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinionsFlywayDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinionsFlywayDemoApplication.class, args);
+	}
+
+}
